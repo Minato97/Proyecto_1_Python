77871e64e817d9fdb8388045294ee22c29d26e68
Ignacio Andrade <ignacio.andrade8033@alumnos.udg.mx>
2022-03-21 21:19:22 -0600

    Busqueda

--- Proyecto1_alfa.py
+++ Proyecto1_alfa.py
@@ -1,29 +1,26 @@
 from os import system
-#Declaración de lista vacia
-lista_estudiantes=[]
+lista_estudiantes= list()
+estudiante = {}
+Id=0
 
-#Declaración de clase con constructor y metodo de mostrar datos
-class Estudiantes:
-    contadorEstudiantes=1
-    def __init__(self, nombre="", edad=0, genero="", promedio=0.0):
-        self.__Nombre=nombre
-        self.__Edad=edad
-        self.__Genero=genero
-        self.__Promedio = promedio
-        self.__ID = Estudiantes.contadorEstudiantes
-        
-    
-    
-    def mostrar(self):
-        print("\nNombre: ",self.__Nombre, " |Edad: ",self.__Edad," |Genero: ",self.__Genero,"|Promedio: ",self.__Promedio,"|ID: ",self.__ID)
-    def getPromedio(self):
-        return self.__Promedio
-#Declaración de metodo crear
+
 def crear():
-    estudianteT=Estudiantes(input("Nombre:"),int(input("Edad:")),input("Genero:"),float(input("Promedio:")))
-    lista_estudiantes.append(estudianteT)
+    estudiante = {}
     
-#Declaración de metodo eliminar
+    Id=len(lista_estudiantes)
+    estudiante["Nombre"] = input("Nombre: ")
+    estudiante["Edad"] = int(input("Edad: "))
+    estudiante["Genero"] = input("Genero: ")
+    estudiante["Promedio"] = float(input("Promedio: "))
+    Id+=1
+    estudiante["Id"] = Id
+    
+    lista_estudiantes.append(estudiante)
+
+def mostrar():
+    for est in lista_estudiantes:
+        print("Nombre: ",est["Nombre"],"- Edad: ",est["Edad"],"- Genero: ",est["Genero"],"- Promedio: ",est["Promedio"],"- ID: ",est["Id"])
+
 def eliminar():
     try:
         elemento=int(input("Ingrese el ID del elemento que desea eliminar: "))
@@ -34,17 +31,17 @@ def eliminar():
     except ValueError:
         print("\n\nOpción inválida\n\n")
         eliminar()
-        
 
-#Declaración de metodo modificar
 def modificar():
     try:
         elemento=int(input("Ingrese el ID del elemento que desea modificar: "))
-        Estudiantes.contadorEstudiantes=elemento
-        estudianteT = Estudiantes(input("Nombre:"),int(input("Edad:")),input("Genero:"),float(input("Promedio:")))
         
-        lista_estudiantes[elemento-1]=estudianteT
-        
+        estudiante["Nombre"] = input("Nombre: ")
+        estudiante["Edad"] = int(input("Edad: "))
+        estudiante["Genero"] = input("Genero: ")
+        estudiante["Promedio"] = float(input("Promedio: "))
+        estudiante["Id"] = elemento
+        lista_estudiantes[elemento-1]=estudiante
         
     except IndexError:
         print("\n\nNo existe registro con el ID ingresado\n\n")
@@ -53,7 +50,6 @@ def modificar():
         print("\n\nOpción inválida\n\n")
         modificar()
 
-#Declaración de metodo ordenar
 def ordenar():
     try:
         print("**********Menú de Ordenamiento*********")
@@ -87,14 +83,28 @@ def ordenar():
 #Declaración de menú de busqueda
 def buscar():
     try:
-        print("**********Menú de Búsqueda*********")
-        print("Presione el número correspondiente según el método de búsqueda que desea implementar:\n\n1. Búsquea secuencial con centinela.\n2. Búsqueda binaria.\n")
+        print("\n**********Menú de Búsqueda*********")
+        print("Presione el número correspondiente según el método de búsqueda que desea implementar:\n\n1. Búsquea secuencial con centinela.\n2. Búsqueda binaria.\n3. Regresar al menú principal.\n")
         opcion=int(input("Por favor ingrese una opción:"))
 
         if opcion==1:
-            busquedaSecuencial()
+            try:
+                num=int(input("\nIngrese el ID del elemento que desea buscar: "))
+                busquedaSecuencial(num)
+                system("pause")
+                buscar()
+            except IndexError:
+                print("\n\nNo existe registro con el ID ingresado\n\n")
+                system("pause")
+                buscar()
+            except ValueError:
+                print("\n\nOpción inválida\n\n")
+                system("pause")
+                buscar()
         elif opcion==2:
             busquedaBinaria()
+        elif opcion==3:
+            menu()
         else:
             print("\n\nOpción inválida\n\n")
             buscar()
@@ -102,57 +112,6 @@ def buscar():
     except ValueError:
         print("\n\nOpción inválida\n\n")
         buscar()
-
-
-#Declaración de funcion de menú principal
-def menu():
-    print("\n")
-    system("pause")
-    system("cls")
-    try:
-        
-        print("\n\n**********Menú Principal*********")
-        print("Presione el número correspondiente según lo que desee hacer:\n\n1. Crear un registro.\n2. Eliminar un registro.\n3. Modificar un registro.\n4. Mostrar registros.\n5. Ordenar los registros.")
-        opcion=int(input("\nPor favor ingrese una opción:"))
-
-        if opcion==1:
-            crear()
-            Estudiantes.contadorEstudiantes+=1
-            menu()
-        elif opcion==2:
-            print("\nLos registros actuales son:\n")
-            for estudiante in lista_estudiantes:
-                estudiante.mostrar()
-            eliminar()
-            print("\nEliminado exitosamente\nLa lista actualizada se muestra a continuación: \n")
-            for estudiante in lista_estudiantes:
-                estudiante.mostrar()
-            menu()
-        elif opcion==3:
-            print("\nLos registros actuales son:\n")
-            for estudiante in lista_estudiantes:
-                estudiante.mostrar()
-            modificar()
-            print("\nLa lista actualizada se muestra a continuación: \n")
-            for estudiante in lista_estudiantes:
-                estudiante.mostrar()
-            menu()
-        elif opcion==4:
-            print("\nLos registros actuales son:\n")
-            for estudiante in lista_estudiantes:
-                estudiante.mostrar()
-            menu()
-        elif opcion==5:
-            ordenar()
-        elif opcion==6:
-            buscar()
-        else:
-            print("\n\nOpción inválida\n\n")
-            menu()
-    except ValueError:
-        print("\n\nOpción inválida\n\n")
-        menu()
-
 
 #Declaración de metodo de ordenamiento
 def burbuja():
@@ -175,15 +134,13 @@ def burbuja():
                     while i < tamano:
                         j= 0
                         while j < (tamano-1):
-                            if lista_estudiantes[j+1].Promedio < lista_estudiantes[j].Promedio:
+                            if lista_estudiantes[j+1]["Promedio"] < lista_estudiantes[j]["Promedio"]:
                                 bucket = lista_estudiantes[j]
                                 lista_estudiantes[j] = lista_estudiantes[j+1]
                                 lista_estudiantes[j+1] = bucket
                         j+= 1
                     i+= 1
-                    k=0
-                    for k in range(k,tamano):
-                        print(lista_estudiantes[k])
+                    mostrar()
                             
                 
                 elif opcion==2: #Burbuja por promedio descendente
@@ -744,11 +701,57 @@ def radixsort():
         print("\n\nOpción inválida\n\n")
         radixsort() 
     
-def busquedaSecuencial():
-    print("encontrado")
+def busquedaSecuencial(num):
+    for est in lista_estudiantes:
+        if num == est["Id"]:
+            print("Nombre: ",est["Nombre"],"- Edad: ",est["Edad"],"- Genero: ",est["Genero"],"- Promedio: ",est["Promedio"],"- ID: ",est["Id"])
+            
 def busquedaBinaria():
     print("encontrado")
     
+
+def menu():
+    print("\n")
+    system("pause")
+    system("cls")
+    try:
+        
+        print("\n\n**********Menú Principal*********")
+        print("Presione el número correspondiente según lo que desee hacer:\n\n1. Crear un registro.\n2. Eliminar un registro.\n3. Modificar un registro.\n4. Mostrar registros.\n5. Ordenar los registros.\n6. Buscar por ID.")
+        opcion=int(input("\nPor favor ingrese una opción:"))
+
+        if opcion==1:
+            crear()
+            menu()
+        elif opcion==2:
+            print("\nLos registros actuales son:\n")
+            mostrar()
+            eliminar()
+            print("\nEliminado exitosamente\nLa lista actualizada se muestra a continuación: \n")
+            mostrar()
+            menu()
+        elif opcion==3:
+            print("\nLos registros actuales son:\n")
+            mostrar()
+            modificar()
+            print("\nLa lista actualizada se muestra a continuación: \n")
+            mostrar()
+            menu()
+        elif opcion==4:
+            print("\nLos registros actuales son:\n")
+            mostrar()
+            menu()
+        elif opcion==5:
+            ordenar()
+        elif opcion==6:
+            buscar()
+        else:
+            print("\n\nOpción inválida\n\n")
+            menu()
+    except ValueError:
+        print("\n\nOpción inválida\n\n")
+        menu()
+
 
 
 print("******Bienvenido*******")
---
+++ personajes = [].py
@@ -0,0 +1,28 @@
+personajes = []
+
+p = { "Nombre":"Aragon","Numero":5}
+
+personajes.append(p)
+
+p = { "Nombre":"ricar","Numero":9}
+personajes.append(p)
+
+print(personajes)
+def burbuja(lista):
+    tamano = len(lista)
+    i= 0
+    while i < tamano:
+        j= 0
+        while j < (tamano-1):
+            if lista[j+1]["Numero"] < lista[j]["Numero"]:
+                bucket = lista[j]
+                lista[j] = lista[j+1]
+                lista[j+1] = bucket
+        j+= 1
+    i+= 1
+    return lista
+
+
+print(burbuja(personajes))
+
+print(personajes[0]["Numero"])
